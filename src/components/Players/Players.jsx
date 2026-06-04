ebf3e6e1f3b60f74b2b6113b9ff8137bb864c288
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-06-04 06:03:49 +0600

    functionality added

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { use } from 'react';
+import { use, useState } from 'react';
 import Availableplayers from './AvialablePlayers/Availableplayers';
+import Selectedplayers from './SelectedPlayers/Selectedplayers';
 
 const Players = ({ playersPromise }) => {
     const playersData = use(playersPromise);
+    const [selectedType, setselectedType] = useState("available"); 
     return (
         <div>
-            <Availableplayers playersData={playersData}></Availableplayers>
+
+            <div className="flex justify-between items-center max-w-7xl mx-auto lg:px-17 px-4 mt-10">
+                {selectedType === "available" ? <h2 className="font-bold text-xl">Available Players</h2> : <h2 className="font-bold text-xl">Selected Players</h2>}
+
+                <div>
+                    <button onClick={() => setselectedType("available")} className={`btn ${selectedType==="available" ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl`}>Available</button>
+                    <button onClick={() => setselectedType("selected")} className={`btn ${selectedType==="selected" ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl`}>Selected</button>
+                </div>
+            </div>
+            {selectedType === "available" ? <Availableplayers playersData={playersData}></Availableplayers> : <Selectedplayers></Selectedplayers>}
         </div>
     );
 };
---
+++ src/components/Players/SelectedPlayers/Selectedplayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Selectedplayers = () => {
+    return (
+        <div>
+            selected players
+        </div>
+    );
+};
+
+export default Selectedplayers;
