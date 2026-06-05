89568432ae4ab7ef6b3f8e43dac8028cd4a9be13
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-06-05 06:01:32 +0600

    footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/logo-footer.png"; // তোমার logo path
+import logo from "../../assets/logo-footer.png"; 
 
 const Footer = () => {
     return (
