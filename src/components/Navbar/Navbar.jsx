91e4b6da242e037e2737224f537dc7f817e932e8
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-06-05 06:23:01 +0600

    problem fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import logo from '../../assets/logo.png';
+import icon from '../../assets/Icon.png';
 
 const Navbar = ({ Coin }) => {
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm px-20">
                 <div className="flex-1">
-                    <img src='/src/assets/logo.png'></img>
+                    <img src={logo} alt="Logo" />
                 </div>
+
                 <div className="flex-none">
                     <button className="flex justify-between items-center gap-2 font-bold text-xl">
                         {Coin} Coins
-                       <img src="/src/assets/Icon.png" alt="Icon" width="20" height="20"></img>
+                        <img src={icon} alt="Icon" width="20" height="20" />
                     </button>
                 </div>
             </div>
